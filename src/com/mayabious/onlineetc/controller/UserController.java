5e197f2473712825443e7e8b0ff50fda5ef3fc2c
Rajib Garai <123@gmail.com>
2016-08-01 09:54:12 +0530

    Add a Class UserController in com.mayabious.controller Package

---
+++ src/com/mayabious/onlineetc/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.mayabious.onlineetc.controller;
+
+public class UserController {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Add a Class UserController in com.mayabious.controller Package");
+
+	}
+
+}
