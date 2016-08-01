7f2d26b7989791d693c712d003010fa9992efde5
Sumit Roy <sumit.roy@gmail.com>
2016-08-01 09:47:44 +0530

    Add a Class TimeController in com.mayabious.controller Package

---
+++ src/com/mayabious/onlineetc/controller/TimeController.java
@@ -0,0 +1,11 @@
+package com.mayabious.onlineetc.controller;
+
+public class TimeController {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Add a Class TimeController in com.mayabious.controller Package");
+
+	}
+
+}
