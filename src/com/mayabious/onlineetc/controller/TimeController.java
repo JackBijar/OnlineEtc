d3eaeafb2d5bed1a6abd352b91ad72e069a0586b
Asit Roy <asit@gmail.com>
2016-08-01 10:52:53 +0530

    Add a New Line on 10:50 AM 01.08.2016 - Commit Message.

--- src/com/mayabious/onlineetc/controller/TimeController.java
+++ src/com/mayabious/onlineetc/controller/TimeController.java
@@ -5,7 +5,7 @@ public class TimeController {
 	public static void main(String[] args) 
 	{
 		System.out.println("Add a Class TimeController in com.mayabious.controller Package");
-
+		System.out.println("Add a New Line, on 10:50 AM 01.08.2016");
 	}
 
 }
